dc34a6eb7df487b5958a95296c08041b3098a7d9
hazemphoenix <hazem.mando408@gmail.com>
2025-07-04 22:43:13 +0300

    feat: create the product abstract class

---
+++ src/Product.java
@@ -0,0 +1,45 @@
+public abstract class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    private Manufacturer manufacturer;
+
+    public Product(String name, double price, int quantity, Manufacturer manufacturer) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.manufacturer = manufacturer;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name)  {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price)  {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity)  {
+        this.quantity = quantity;
+    }
+
+    public Manufacturer getManufacturer() {
+        return this.manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer)  {
+        this.manufacturer = manufacturer;
+    }
+}
