43f711dd5135583081d81e24a882fb354ec3f5ab
hazemphoenix <hazem.mando408@gmail.com>
2025-07-04 22:46:33 +0300

    feat: create the manufacturer class

---
+++ src/Manufacturer.java
@@ -0,0 +1,36 @@
+public class Manufacturer {
+    private String name;
+    private String address;
+    private Sector sector;
+
+    Manufacturer(String name, String address, Sector sector) {
+        this.name = name;
+        this.address = address;
+        this.sector = sector;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public Sector getSector() {
+        return this.sector;
+    }
+
+    public void setSector(Sector sector){
+        this.sector = sector;
+    }
+
+}
