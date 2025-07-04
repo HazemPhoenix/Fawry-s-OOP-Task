53c067ae41ae0dd11716cd2accc739cc488703c6
hazemphoenix <hazem.mando408@gmail.com>
2025-07-04 23:06:20 +0300

    test: Manufacturer class and BiscuitFlavor enum

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Sector s;
-        s = Sector.Food;
-        System.out.println(s);
+        BiscuitFlavor bf = BiscuitFlavor.Chocolate;
+        BiscuitFlavor bf2 = BiscuitFlavor.Vanilla;
+        BiscuitFlavor bf3 = BiscuitFlavor.Butter;
+        System.out.println(bf);
+        System.out.println(bf2);
+        System.out.println(bf3);
+
+        Manufacturer m = new Manufacturer("Samsung", "Suwon-si, South Korea", Sector.Technology);
+        System.out.println(m.getAddress());
+        System.out.println(m.getName());
+        System.out.println(m.getSector());
+        m.setAddress("Cairo, Egypt");
+        System.out.println(m.getAddress());
+
     }
 }
